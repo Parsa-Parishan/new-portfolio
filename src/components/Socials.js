0cb33a39587f339262edaea478d1bc7be5a80c80
Parsa Parishan <pparsaparishan@yahoo.com>
2023-05-12 16:10:26 -0400

    add list of socials

---
+++ src/components/Socials.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaInstagram,
+  FaRegEnvelope,
+} from "react-icons/fa";
+
+export default function Socials() {
+  return (
+    <div className="socials">
+      <ul>
+        <li>
+          <a href="https://github.com/Parsa-Parishan">
+            <FaGithub />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/parsa-parishan-419117252/"
+            target="_blank"
+          >
+            <FaLinkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/parsa_parishan/">
+            <FaInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:parsa.parishan2017@gmail.com">
+            <FaRegEnvelope />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
