9af5ee52abd2f00b1d21fab46a9db27d5106ee1f
Parsa Parishan <pparsaparishan@yahoo.com>
2023-11-14 20:26:45 -0500

    add project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,67 +7,120 @@ import kanban from "../img/screenshots/kanban.png";
 import form from "../img/screenshots/form.jpg";
 import { FaGithub } from "react-icons/fa";
 
+const projectsData = [
+  {
+    id: 1,
+    name: "Ecommerce website",
+    image: ecom,
+    description:
+      "E-commerce website for vintage clothes. Integrated shopping cart to the web app, cart summary, and preview. A list of products and utilized React Router for each page such as login, creating an account, and a page for each product.",
+    techStack: [
+      "HTML/CSS",
+      "JavaScript",
+      "React JS",
+      "React Router",
+      "Framer Motion",
+    ],
+    githubLink: "https://github.com/Parsa-Parishan/my-shop",
+    liveLink: "https://parsa-parishan.github.io/my-shop/",
+  },
+  {
+    id: 2,
+    name: "Multi-step Form",
+    image: form,
+    description:
+      "A multi-step form, each component is a step of a sequence to select the type of a plan an extra add-ons with the plan, a summary of plan and add-ons.",
+    techStack: ["HTML/CSS", "JavaScript", "React JS", "SASS"],
+    githubLink: "https://github.com/Parsa-Parishan/form",
+    liveLink: "http://parsa-parishan.github.io/form/",
+  },
+  {
+    id: 3,
+    name: "Link Sharing",
+    image: form,
+    description: "A fully functioanl link sharing app created with ReactJS.",
+    techStack: ["HTML/CSS", "JavaScript", "React JS", "SASS"],
+    githubLink: "https://github.com/Parsa-Parishan/link_sharing",
+    liveLink: "https://parsa-parishan.github.io/link_sharing/",
+  },
+  {
+    id: 4,
+    name: "Old Portfolio",
+    image: oldPortfolio,
+    description: "A fully functioanl link sharing app created with ReactJS.",
+    techStack: ["HTML/CSS", "JavaScript", "React JS", "SASS"],
+    githubLink: "https://github.com/Parsa-Parishan/My-Portfolio",
+    liveLink: "https://parsa-parishan.github.io/My-Portfolio/",
+  },
+  {
+    id: 5,
+    name: "Current weather/forecast app",
+    image: weather,
+    description:
+      "A weather forecast app, showing current weather and the next five days. The app utilizes Weather API and GEO API for the cities data. With implemented async pagination for a better user experience with city suggestions.",
+    techStack: [
+      "HTML/CSS",
+      "JavaScript",
+      "React JS",
+      "SASS",
+      "GEO API",
+      "WEATHER API",
+    ],
+    githubLink: "https://github.com/Parsa-Parishan/My-Weather-App",
+    liveLink: "https://parsa-parishan.github.io/My-Weather-App/",
+  },
+  {
+    id: 6,
+    name: "kanban",
+    image: kanban,
+    description:
+      "Organize the daily task with the ability to drag and drop from one column to another.",
+    techStack: ["HTML/CSS", "JavaScript"],
+    githubLink: "https://github.com/Parsa-Parishan/kanban-board",
+    liveLink: "https://parsa-parishan.github.io/kanban-board/",
+  },
+];
+
 export default function Projects() {
-  const [commerse, setCommerse] = useState(false);
   const [header, setHeader] = useState(false);
-  const [first, setFirst] = useState(false);
-  const [second, setSecond] = useState(false);
-  const [third, setThird] = useState(false);
-  const [fourth, setFourth] = useState(false);
-  const [fifth, setFifth] = useState(false);
-  const [sixth, setSixth] = useState(false);
-  const [modalOne, setModalOne] = useState(false);
-  const [modalTwo, setModalTwo] = useState(false);
-  const [modalThree, setModalThree] = useState(false);
-  const [modalFour, setModalFour] = useState(false);
-  const [modalFive, setModalFive] = useState(false);
-  const [modalSix, setModalSix] = useState(false);
+
+  const [scrollPositions, setScrollPositions] = useState(Array(6).fill(false));
+  const [modalOpen, setModalOpen] = useState(Array(6).fill(false));
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY >= 1036) {
-        setHeader(() => true);
-      }
-      if (window.scrollY >= 1170) {
-        setFirst(() => true);
-      }
-      if (window.scrollY >= 1430) {
-        setSecond(() => true);
-      }
-      if (window.scrollY >= 1710) {
-        setThird(() => true);
-      }
-      if (window.scrollY >= 1980) {
-        setFourth(() => true);
-      }
-      if (window.scrollY >= 2190) {
-        setFifth(() => true);
-      }
-      if (window.scrollY >= 2280) {
-        setSixth(() => true);
-      }
-    });
-    window.addEventListener("click", (e) => {
-      if (e.target.classList.contains("project")) {
-        setModalOne(() => false);
-        setModalTwo(() => false);
-        setModalThree(() => false);
-        setModalFour(() => false);
-        setModalFive(() => false);
-        setModalSix(() => false);
-      }
-    });
+    const handleScroll = () => {
+      const positions = [
+        1036, // Adjust these values based on your layout
+        1170,
+        1430,
+        1710,
+        1980,
+        2190,
+        2280,
+      ];
+      const updatedScrollPositions = positions.map(
+        (pos) => window.scrollY >= pos
+      );
+      setScrollPositions(updatedScrollPositions);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
+  const handleModalClose = () => {
+    setModalOpen(Array(6).fill(false));
+  };
+
+  const handleModalOpen = (index) => {
+    setModalOpen((prevState) => prevState.map((value, i) => i === index));
+  };
+
   const body = document.body;
-  if (
-    modalOne ||
-    modalTwo ||
-    modalThree ||
-    modalFour ||
-    modalFive ||
-    modalSix
-  ) {
+  if (modalOpen.some((open) => open)) {
     body.classList = "hidden-overflow";
   } else {
     body.classList = "";
@@ -81,261 +134,53 @@ export default function Projects() {
         </h2>
       </div>
       <div className="show-case">
-        <div
-          className={`project ecommerce ${modalOne && "modal"}`}
-          onClick={() => setModalOne(() => true)}
-        >
-          <div className={`image-wrapper ${first && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={ecom} alt="ecommerce" />
+        {projectsData.map((project, index) => (
+          <div
+            key={project.id}
+            className={`project ${project.name
+              .toLowerCase()
+              .replace(/\s/g, "-")} ${modalOpen[index] && "modal"}`}
+            onClick={() => handleModalOpen(index)}
+          >
+            <div
+              className={`image-wrapper ${
+                scrollPositions[index] && "project-transition"
+              }`}
+            >
+              <div className="image">
+                <div className="filter"></div>
+                <img src={project.image} alt={project.name} />
+              </div>
+            </div>
+            <div
+              className={`caption ${
+                scrollPositions[index] && "name-transition"
+              }`}
+            >
+              <div className="caption-header">
+                <h2>{project.name}</h2>
+              </div>
+              <div className="caption-paragraph">
+                <p>{project.description}</p>
+              </div>
+              <div className="caption-tech">
+                <ul>
+                  {project.techStack.map((tech, i) => (
+                    <li key={i}>{tech}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="links">
+                <a href={project.githubLink} target="_blank">
+                  <FaGithub />
+                </a>
+                <a href={project.liveLink} target="_blank">
+                  Live
+                </a>
+              </div>
             </div>
           </div>
-          <div className={`caption ${first && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Ecommerce website</h2>
-            </div>
-            <div className="caption-paragraph">
-              <p>
-                E-commerce website for vintage clothes. Integrated shopping cart
-                to the web app, cart summary, and preview. A list of products
-                and utilized React Router for each page such as login, creating
-                An account, and a page for each product.
-              </p>
-            </div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML.CSS</li>
-                <li>JAVASCRIPT</li>
-                <li>REACT JS</li>
-                <li>REACT ROUTER</li>
-                <li>FRAMER-MOTION</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a
-                href="https://github.com/Parsa-Parishan/my-shop"
-                target="_blank"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://parsa-parishan.github.io/my-shop/"
-                target="_blank"
-              >
-                Live
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={`project form ${modalTwo && "modal"}`}
-          onClick={() => setModalTwo(() => true)}
-        >
-          <div className={`image-wrapper ${second && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={form} alt="form" />
-            </div>
-          </div>
-          <div className={`caption ${second && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Multi-step Form</h2>
-            </div>
-            <div className="caption-paragraph">
-              <p>
-                A multi-step form, each component is step of a sequence to
-                select type of a plan an extra add-ons with the plan, a summary
-                of plan and add-ons
-              </p>
-            </div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML.CSS</li>
-                <li>JAVASCRIPT</li>
-                <li>REACT JS</li>
-                <li>SASS</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a href="https://github.com/Parsa-Parishan/form" target="_blank">
-                <FaGithub />
-              </a>
-              <a href="http://parsa-parishan.github.io/form/" target="_blank">
-                Live
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={`project oldPortfolio ${modalThree && "modal"}`}
-          onClick={() => setModalThree(() => true)}
-        >
-          <div className={`image-wrapper ${third && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={oldPortfolio} alt="old portfolio" />
-            </div>
-          </div>
-          <div className={`caption ${third && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Old Portfolio</h2>
-            </div>
-            <div className="caption-paragraph"></div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML.CSS</li>
-                <li>JAVASCRIPT</li>
-                <li>REACT JS</li>
-                <li>FRAMER-MOTION</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a
-                href="https://github.com/Parsa-Parishan/My-Portfolio"
-                target="_blank"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://parsa-parishan.github.io/My-Portfolio/"
-                target="_blank"
-              >
-                Live
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={`project api ${modalFour && "modal"}`}
-          onClick={() => setModalThree(() => true)}
-        >
-          <div className={`image-wrapper ${fourth && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={weather} alt="weather api" />
-            </div>
-          </div>
-          <div className={`caption ${fourth && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Current weather/forecast app</h2>
-            </div>
-            <div className="caption-paragraph">
-              <p>
-                A weather forecast app, showing current weather and the next
-                five days. The app utilizes Weather API and GEO API for the
-                cities data. With implemented async pagination for a better user
-                experience with city suggestions.
-              </p>
-            </div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML/CSS</li>
-                <li>JAVASCIRPT</li>
-                <li>REACT JS</li>
-                <li>WEATHER API</li>
-                <li>GEO API</li>
-                <li>SASS</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a
-                href="https://github.com/Parsa-Parishan/My-Weather-App"
-                target="_blank"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://parsa-parishan.github.io/My-Weather-App/"
-                target="_blank"
-              >
-                Live
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={`project kanban ${modalFive && "modal"}`}
-          onClick={() => setModalFive(() => true)}
-        >
-          <div className={`image-wrapper ${fifth && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={kanban} alt="kanban board" />
-            </div>
-          </div>
-          <div className={`caption ${fifth && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Kanban Board</h2>
-            </div>
-            <div className="caption-paragraph">
-              <p>
-                Organize the daily task with the ability to drag and drop from
-                one column to another.
-              </p>
-            </div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML/CSS</li>
-                <li>JAVASCRIPT</li>
-                <li>REACT JS</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a
-                href="https://github.com/Parsa-Parishan/kanban-board"
-                target="_blank"
-              >
-                <FaGithub />
-              </a>
-              <a target="_blank">Live</a>
-            </div>
-          </div>
-        </div>
-        <div
-          className={`project filler ${modalSix && "modal"}`}
-          onClick={() => setModalSix(() => true)}
-        >
-          <div className={`image-wrapper ${sixth && "project-transition"}`}>
-            <div className="image">
-              <div className="filter"></div>
-              <img src={note} alt="note" />
-            </div>
-          </div>
-          <div className={`caption ${sixth && "name-transition"}`}>
-            <div className="caption-header">
-              <h2>Note App</h2>
-            </div>
-            <div className="caption-paragraph">
-              <p>
-                Note Taking app is built in React JS. The main function of this
-                app is creating notes, editing notes and deleting notes.
-              </p>
-            </div>
-            <div className="caption-tech">
-              <ul>
-                <li>HTML/CSS</li>
-                <li>JAVASCRIPT</li>
-                <li>REACT JS</li>
-                <li>REACT RESPONSIVE MODAL</li>
-              </ul>
-            </div>
-            <div className="links">
-              <a
-                href="https://github.com/Parsa-Parishan/Note-App"
-                target="_blank"
-              >
-                <FaGithub />
-              </a>
-              <a
-                href="https://parsa-parishan.github.io/Note-App/"
-                target="_blank"
-              >
-                Live
-              </a>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
